cda6aa23e90b95bf74790be66728ff6ca4bf33bf
Massoulier <kalo@macbook-pro-de-christophe-1.home>
2025-03-12 14:44:57 +0100

    Passage en mode Webhook pour éviter le conflit 409

--- smart_money_bot.py
+++ smart_money_bot.py
@@ -85,6 +85,9 @@ def webhook():
 
 # Lancement du bot et du serveur Flask
 if __name__ == "__main__":
-    import threading
-    threading.Thread(target=lambda: bot.polling()).start()
+    WEBHOOK_URL = "https://web-production-3f98.up.railway.app/webhook"
+
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+    
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
